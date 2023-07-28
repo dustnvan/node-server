5f5931d924e7a2edb8d044fa6391d131d60568bd
dustoonv <dustinvans7@gmail.com>
2023-07-27 21:55:41 -0500

    using express

--- index.js
+++ index.js
@@ -1,93 +1,90 @@
-const http = require('http');
+// Vanilla Node
+
+// const http = require('http');
+// const path = require('path');
+// const fs = require('fs');
+
+
+// const server = http.createServer((req, res) => {
+//   // Build file path
+//   let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+
+//   console.log(req.url);
+
+//   // Extension of file
+//   let extname = path.extname(filePath);
+
+//   // Initial content type
+//   let contentType = 'text/html';
+
+//   //Check ext and set content type
+//   switch (extname) {
+//     case '.js':
+//       contentType = 'text/javascript';
+//       break;
+//     case '.css':
+//       contentType = 'text/css';
+//       break;
+//     case '.json':
+//       contentType = 'application/json';
+//       break;
+//     case '.png':
+//       contentType = 'image/png';
+//       break;
+//     case '.jpg':
+//       contentType = 'image/jpg';
+//       break;
+
+//   }
+
+//   // Read File
+//   fs.readFile(filePath, (err, content) => {
+//     if (err) {
+//       if (err.code == 'ENOENT') {
+//         // Page not found
+//         fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+//           res.writeHead(200, { 'Content-Type': 'text/html' });
+//           res.end(content, 'utf8');
+//         });
+//       }
+//       else {
+//         // Some server error
+//         res.writeHead(500);
+//         res.end(`Server Error: ${err.code}`);
+//       }
+//     }
+//     else {
+//       //Success 
+//       res.writeHead(200, { 'Content-Type': contentType });
+//       res.end(content, 'utf8');
+//     }
+//   });
+// });
+
+// const PORT = process.env.PORT || 5000;
+
+// server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// Express
+const express = require('express');
 const path = require('path');
-const fs = require('fs');
+const logger = require('./middleware/logger');
 
-const server = http.createServer((req, res) => {
-  // if (req.url === '/') {
-  //   fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
-  //     if (err) throw err;
+const app = express();
 
-  //     res.writeHead(200, { 'Content-Type': 'text/html' });
-  //     res.end(content);
-  //   });
-  // }
+// Init Middle
+// app.use(logger);
 
-  // if (req.url === '/about') {
-  //   fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
-  //     if (err) throw err;
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-  //     res.writeHead(200, { 'Content-Type': 'text/html' });
-  //     res.end(content);
-  //   });
-  // }
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
-  // if (req.url === '/api/users') {
-  //   const users = [
-  //     { name: 'Bob Smith', age: 40 },
-  //     { name: 'John Doe', age: 30 }
-  //   ];
-
-  //   res.writeHead(200, { 'Content-Type': 'application/json' });
-  //   res.end(JSON.stringify(users));
-  // }
-
-  // Build file path
-  let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
-
-  console.log(req.url);
-
-  // Extension of file
-  let extname = path.extname(filePath);
-
-  // Initial content type
-  let contentType = 'text/html';
-
-  //Check ext and set content type
-  switch (extname) {
-    case '.js':
-      contentType = 'text/javascript';
-      break;
-    case '.css':
-      contentType = 'text/css';
-      break;
-    case '.json':
-      contentType = 'application/json';
-      break;
-    case '.png':
-      contentType = 'image/png';
-      break;
-    case '.jpg':
-      contentType = 'image/jpg';
-      break;
-
-  }
-
-  // Read File
-  fs.readFile(filePath, (err, content) => {
-    if (err) {
-      if (err.code == 'ENOENT') {
-        // Page not found
-        fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
-          res.writeHead(200, { 'Content-Type': 'text/html' });
-          res.end(content, 'utf8');
-        });
-      }
-      else {
-        // Some server error
-        res.writeHead(500);
-        res.end(`Server Error: ${err.code}`);
-      }
-    }
-    else {
-      //Success 
-      res.writeHead(200, { 'Content-Type': contentType });
-      res.end(content, 'utf8');
-    }
-
-
-  });
-});
+// Member API Routes
+app.use('/api/members', require('./routes/api/members'));
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
